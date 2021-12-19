86212ab956af9f2e317557f448d0daf4891d0f20
matheusferreira1940@outlook.com <matheusferreira1940@outlook.com>
2021-12-19 13:43:51 -0300

    fix/image-src

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,11 +3,13 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
 const Thumbnail = forwardRef(({ result }, ref) => {
+  const BASE_URL = "https://image.tmdb.org./t/p/original/";
+
   return (
     <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image layout="responsive" 
-      src={`https://image.tmdb.org./t/p/original/${result.backdrop_path}`} alt={result.title}
-      height={1080} width={1920} quality={100} />
+      src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} alt={result.title}
+      height={1080} width={1920} />
       <div className="p-2 ">
         <p className="truncate max-w-md">{result.overview}</p>
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
