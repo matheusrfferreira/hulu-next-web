c132d1509c3a0d7f3dfc31217b8cb0bb2546c335
matheusferreira1940@outlook.com <matheusferreira1940@outlook.com>
2021-12-19 13:21:08 -0300

    fix/image-src

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -8,8 +8,8 @@ const Thumbnail = forwardRef(({ result }, ref) => {
   return (
     <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image layout="responsive" 
-      src={`${BASE_URL}${result.backdrop_path || result.poster_path}`} alt={result.title}
-      height={1080} width={1920} />
+      src={`${BASE_URL}${result.backdrop_path}`} alt={result.title}
+      height={1080} width={1920} quality={100} />
       <div className="p-2 ">
         <p className="truncate max-w-md">{result.overview}</p>
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
